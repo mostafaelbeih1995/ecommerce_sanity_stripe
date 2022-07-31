5b83b0312eb64bdf8d80c962d58cfec32298da10
Mostafa Elbeih <mte10@mail.aub.edu>
2022-07-31 02:18:54 +0200

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from "react-icons/ai";
+import Cart  from "./Cart";
+import { useStateContext } from '../context/StateContext';
 
-const NavBar = () => {
+
+const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
+
     <div className='navbar-container'>
       <p className='logo'>
         <Link href="/">
           Online headphones
         </Link>
       </p>
-      <button type='button' className='cart-icon' onClick="">
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className= "cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-    </div>
-  )
-}
 
-export default NavBar
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
